5076d5534f2d34a4c452a3c5e7b5d2cf984c20f7
linoe97 <nicola.pace97@gmail.com>
2020-05-07 11:43:29 +0200

    added frequency divider

---
+++ frequency_divider.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL; 
+ 
+entity frequency_divider is
+    Port ( clk_in: in std_logic;				 -- clock input
+           reset: in std_logic;	       -- reset input 
+           clk_out: out std_logic		 -- output
+     );
+end frequency_divider;
+
+architecture Behavioral of frequency_divider is
+	
+	signal temporal: STD_LOGIC
+	signal counter : integer range 0 to 15;
+	
+	begin
+	freq_div: process (reset, clk_in) begin
+	   if (reset = '1') then
+            temporal <= '0';
+            counter <= 0;
+		 elsif rising_edge(clk) then
+			  if (counter= 15) then
+					counter <= 0;
+					temporal <= NOT(temporal);
+			  else 
+					counter <= counter + 1;
+			  end if;
+		 end if;
+	end process;
+	
+	clk_out <= temporal;
+end Behavioral;
